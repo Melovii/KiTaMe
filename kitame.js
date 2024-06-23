bd7751d34361058471a5058331a2b19766fd352b
Melovii <145908254+Melovii@users.noreply.github.com>
2024-06-23 23:48:00 +0300

    finished the UI and integrated Styles

--- kitame.js
+++ kitame.js
@@ -2,86 +2,219 @@
 
 console.log("Initiate Code 2");
 
-let rounds = parseInt(prompt("How many rounds do you want to play KiTaMe?"));
-console.log("Rounds: " + rounds);
 let playerScore = 0;
 let computerScore = 0;
 
-playGame(rounds);
+// rock = tanuki //
+// paper = me //
+// scissors = kitsune //
 
-function playGame(numOfRounds) {
+const fox = document.createElement("img");
+fox.src = "../images/fox.png";
+fox.style.width = "250px";
+fox.style.height = "auto";
 
-    for (let i = 0; i < numOfRounds; i++) {
-        let computerChoice1 = getComputerChoice();
-        console.log("Computer Choice: " + computerChoice1);
+const eye = document.createElement("img");
+eye.src = "../images/eye.png";
+eye.style.width = "250px";
+eye.style.height = "auto";
 
-        let playerChoice1 = getPlayerChoice();
-        console.log("Player Choice: " + playerChoice1);
+const raccoon = document.createElement("img");
+raccoon.src = "../images/raccoon.png";
+raccoon.style.width = "250px";
+raccoon.style.height = "auto";
 
-        playRound(playerChoice1, computerChoice1);
-    }
-    alert("Your Score: " + playerScore + "\nPC Score: " + computerScore);
-}
+const box1 = document.querySelector(".box1");
+box1.setAttribute("class", "box");
+box1.appendChild(fox);
+
+const box2 = document.querySelector(".box2");
+box2.setAttribute("class", "box");
+box2.appendChild(eye);
+
+const box3 = document.querySelector(".box3");
+box3.setAttribute("class", "box");
+box3.appendChild(raccoon);
+
+
+// -------------
+
+const option1 = document.createElement("button");
+option1.setAttribute("class", "options");
+option1.textContent = "Kitsune";
+option1.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    console.log(`Computer choice: ${computerChoice}`);
+    const playerChoice = option1.textContent.toLowerCase()
+    console.log(`Player choice: ${playerChoice}`);
+    playRound(playerChoice, computerChoice);
+})
+
+// TODO: MAKE SET WINNER METHOD WHICH CHECKS WINNER STATE + PLAYED CHARACTERS + IMG
+
+const option2 = document.createElement("button");
+option2.setAttribute("class", "options");
+option2.textContent = "Me";
+option2.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    console.log(`Computer choice: ${computerChoice}`);
+    const playerChoice = option2.textContent.toLowerCase()
+    console.log(`Player choice: ${playerChoice}`);
+    playRound(playerChoice, computerChoice);
+})
+
+const option3 = document.createElement("button");
+option3.setAttribute("class", "options");
+option3.textContent = "Tanuki";
+option3.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    console.log(`Computer choice: ${computerChoice}`);
+    const playerChoice = option3.textContent.toLowerCase()
+    console.log(`Player choice: ${playerChoice}`);
+    playRound(playerChoice, computerChoice);
+})
+
+box1.appendChild(option1);
+box2.appendChild(option2);
+box3.appendChild(option3);
+
+
+const results = document.querySelector('.winner');
+results.style.justifyContent = 'space-evenly';
+
+const yourChoice = document.createElement('div');
+const playerIMG = document.createElement('img');
+playerIMG.setAttribute('class', 'elements');
+yourChoice.textContent = 'Your Choice';
+yourChoice.setAttribute('class', 'resultStyle');
+results.appendChild(yourChoice);
+
+const winnerDIV = document.createElement('div');
+const winnerIMG = document.createElement('img');
+winnerDIV.textContent = 'Winner';
+winnerIMG.setAttribute('class', 'elements');
+winnerDIV.setAttribute("class", "resultStyle");
+results.appendChild(winnerDIV);
+
+const theirChoice = document.createElement('div');
+const pcIMG = document.createElement('img');
+theirChoice.textContent = "PC's Choice";
+pcIMG.setAttribute('class', 'elements');
+theirChoice.setAttribute('class', 'resultStyle');
+results.appendChild(theirChoice);
+
 
 function playRound(playerChoice, computerChoice) {
     let winner = undefined;
     switch (playerChoice) {
-        case "kitsune": switch(computerChoice) {
-            case "kitsune": winner = 2;
-            break;
-            case "tanuki": winner = 1;
-            break;
-            case "me": winner = 0;
-            break;
-            default: console.log("somehow an error occured");
-
+        case "kitsune": {
+            playerIMG.src = '../images/fox.png';
+            yourChoice.textContent = 'You Chose KITSUNE!';
+            switch(computerChoice) {
+                case "kitsune": {
+                    pcIMG.src = '../images/fox.png';
+                    theirChoice.textContent = "The PC Chose KITSUNE!";
+                    winner = 2;
+                    break;
+                }
+                case "tanuki": {
+                    pcIMG.src = '../images/raccoon.png';
+                    theirChoice.textContent = "The PC Chose TANUKI!";
+                    winner = 1;
+                    break;
+                }
+                case "me": {
+                    pcIMG.src = '../images/eye.png';
+                    theirChoice.textContent = "The PC Chose ME!";
+                    winner = 0;
+                    break;
+                }
+                default: console.log("error occured with computerChoice switch case");
+            }
         }
         break;
-        case "tanuki": switch(computerChoice) {
-            case "kitsune": winner = 0; break;
-            case "tanuki": winner = 2; break;
-            case "me": winner = 1; break;
-            default: console.log("somehow an error occured");
+        case "tanuki": {
+            playerIMG.src = '../images/raccoon.png';
+            yourChoice.textContent = 'You Chose TANUKI!';
+            switch (computerChoice) {
+                case "kitsune": {
+                    pcIMG.src = '../images/fox.png'
+                    theirChoice.textContent = "The PC Chose KITSUNE!";
+                    winner = 0;
+                    break;
+                }
+                case "tanuki": {
+                    pcIMG.src = '../images/raccoon.png'
+                    theirChoice.textContent = "The PC Chose TANUKI!";
+                    winner = 2;
+                    break;
+                }
+                case "me": {
+                    pcIMG.src = '../images/eye.png'
+                    theirChoice.textContent = "The PC Chose ME!";
+                    winner = 1;
+                    break;
+                }
+                default:
+                    console.log("error occured with computerChoice switch case");
+            }
         }
         break;
-        case "me": switch(computerChoice) {
-            case "kitsune": winner = 1; break;
-            case "tanuki": winner = 0; break;
-            case "me": winner = 2; break;
-            default: console.log("somehow an error occured");
+        case "me":
+        {
+            playerIMG.src = '../images/eye.png';
+            yourChoice.textContent = 'You Chose ME!';
+            switch(computerChoice) {
+                case "kitsune": {
+                    pcIMG.src = '../images/fox.png'
+                    theirChoice.textContent = "The PC Chose KITSUNE!";
+                    winner = 1;
+                    break;
+                }
+                case "tanuki": {
+                    pcIMG.src = '../images/raccoon.png'
+                    theirChoice.textContent = "The PC Chose TANUKI!";
+                    winner = 0;
+                    break;
+                }
+                case "me": {
+                    pcIMG.src = '../images/eye.png'
+                    theirChoice.textContent = "The PC Chose ME!";
+                    winner = 2;
+                    break;
+                }
+                default: console.log("error occured with computerChoice switch case");
+            }
         }
         break;
-        default: console.log("somehow an error occured");
+        default: console.log("error occured with playerChoice switch case");
     }
 
     if (winner == 0) {
-        alert("LET'S GOOO YOU WON BRO");
+        console.log("You Won");
+        winnerDIV.textContent = "LETS GOOO YOU WON BRO";
         playerScore++;
     } else if (winner == 1) {
-        alert("Damn, didn't know you sucked hard");
+        console.log("The PC Won");
+        winnerDIV.textContent = "Damn, you suck.";
         computerScore++;
     } else if (winner == 2) {
-        alert("gahdam it's a draw");
+        console.log("It's a Draw!");
+        winnerDIV.textContent = "gahdam it's a draw";
     } else {
-        console.log("somehow an error occured");
-        // console.log(e.stack);
+        console.log("error occured with winner declaration");
     }
-}
 
-function getPlayerChoice() {
-    do {
-        Hchoice = prompt("Kitsune, Tanuki, or Me?");
-        Hchoice = Hchoice.toLowerCase();
-    } while (Hchoice != "kitsune" && Hchoice != "tanuki" && Hchoice != "me");
-
-    switch (Hchoice) {
-        case "kitsune": return Hchoice; break;
-        case "tanuki": return Hchoice; break;
-        case "me": return Hchoice; break;
-        default: {
-            console.log("BRUDAH ENTER CORRECT NAME SMH");
-            getPlayerChoice();
-        }
+    if (computerScore > 5) {
+        winnerDIV.textContent = "L THE PC WON LMAO";
+        option1.disabled = true;
+        option2.disabled = true;
+        option3.disabled = true;
+    } else if (playerScore > 5) {
+        winnerDIV.textContent = "Nice win bro.";
+        option1.disabled = true;
+        option2.disabled = true;
+        option3.disabled = true;
     }
 }
 
